8aac7b542502fd4ed3e98911dd907f4870a5a737
Heitor Boschirolli <heitor.boschirolli@office.vulcanet.com.br>
2019-09-09 19:14:58 -0300

    clone function working

--- llvm/lib/Transforms/Hi/Hi.cpp
+++ llvm/lib/Transforms/Hi/Hi.cpp
@@ -4,6 +4,25 @@
 #include "llvm/IR/Function.h"
 #include "llvm/Pass.h"
 #include "llvm/Support/raw_ostream.h"
+
+#include "llvm/Transforms/Utils/Cloning.h"
+#include "llvm/ADT/STLExtras.h"
+#include "llvm/ADT/SmallPtrSet.h"
+#include "llvm/Analysis/DomTreeUpdater.h"
+#include "llvm/Analysis/LoopInfo.h"
+#include "llvm/AsmParser/Parser.h"
+#include "llvm/IR/Argument.h"
+#include "llvm/IR/Constant.h"
+#include "llvm/IR/DIBuilder.h"
+#include "llvm/IR/DebugInfo.h"
+#include "llvm/IR/Function.h"
+#include "llvm/IR/IRBuilder.h"
+#include "llvm/IR/InstIterator.h"
+#include "llvm/IR/Instructions.h"
+#include "llvm/IR/IntrinsicInst.h"
+#include "llvm/IR/LLVMContext.h"
+#include "llvm/IR/Module.h"
+#include "llvm/IR/Verifier.h"
 using namespace llvm;
 
 namespace {
@@ -14,12 +33,28 @@ namespace {
         bool runOnModule(Module &M) override {
             errs() << "Hi: " << '\n';
 
-            auto ftype = M.getTypeByName("another_function");
-            for (auto &F : M) {
-                errs() << F.getName() << '\n';
-            }
+            // get the function to be cloned
+            Function *OldFunc = M.getFunction("another_function");
 
+            // get necessary info about the function
+            FunctionType *ftype = OldFunc->getFunctionType();
+
+            // insert new function
             M.getOrInsertFunction("the_new_function", ftype);
+
+            // set parameters to clone OldFunction into NewFunction
+            ValueToValueMapTy VMap;
+
+            // create new function
+            Function *NewFunction = CloneFunction(OldFunc, VMap);
+            NewFunction->setName("the_new_another_function");
+
+            // check if the function was cloned properly
+            // for (auto &F : M) {
+            //     errs() << F.getName() << '\n';
+            // }
+
+            M.dump();
 
             return true;
         }
